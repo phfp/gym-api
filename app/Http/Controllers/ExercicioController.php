35121dadcf5f85492e9429ab2eaa39374d176e63
Paulo Henrique <paulohfprates@gmil.com>
2019-08-17 00:59:32 -0300

    ExerciciosController - Validacao

--- app/Http/Controllers/ExercicioController.php
+++ app/Http/Controllers/ExercicioController.php
@@ -11,6 +11,11 @@ class ExercicioController extends Controller
 {
     function exercicios(){
         $exercicios = Exercicio::all();
+
+        foreach ($exercicios as &$exercicio) {
+            $exercicio->imagem = asset($exercicio->imagem);
+        }
+
         return $exercicios;
     }
 
@@ -18,12 +23,74 @@ class ExercicioController extends Controller
 
         $data = $request->all();
 
+        Validator::extend('base64image', function($attribute, $value, $parameters, $validator){
+            $explode = explode(',', $value);
+            $allow = ['png', 'jpg', 'svg', 'jpeg'];
+            $format = str_replace(
+                [
+                    'data:image/',
+                    ';',
+                    'base64',
+                ],
+                [
+                    '', '', '',
+                ],
+                $explode[0]
+            );
+
+            if(!in_array($format, $allow)){
+                return false;
+            }
+
+            if(!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])){
+                return false;
+            }
+            return true;
+        });
+
+
+        $validacao = Validator::make($data, [
+            'imagem' => 'base64image',
+            'descricao' => ['required', 'string'],
+            'grupo_exercicio_id' => ['required'],
+        ], ['base64image' => 'Imagem inválida!']);
+
+        if($validacao->fails()){
+            return $validacao->errors();
+        }
+
+
+        $time = time();
+        $diretorioPai = 'exercicios';
+        $diretorioImagem = $diretorioPai.DIRECTORY_SEPARATOR.'grupo'.$data['grupo_exercicio_id'];
+        $ext = substr($data['imagem'], 11, strpos($data['imagem'], ';') - 11);
+        $urlImagem = $diretorioImagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
+
+        $file = str_replace('data:image/'.$ext.';base64,','',$data['imagem']);
+        $file = base64_decode($file);
+
+        if(!file_exists($diretorioPai)){
+            mkdir($diretorioPai,0700);
+        }
+
+        if(!file_exists($diretorioImagem)){
+            mkdir($diretorioImagem,0700);
+        }
+
+        if(file_exists($data['imagem'])){
+            unlink($data['imagem']);
+        }
+
+        file_put_contents($urlImagem,$file);
+        
+        $data['imagem'] = $urlImagem;
+        
         $exercicio = Exercicio::create([
             'descricao' => $data['descricao'],
             'grupo_exercicio_id' => $data['grupo_exercicio_id'],
-            'imagem' => $data['imagem']
-        ]);
+            'imagem' => $data['imagem'],
+        ]);        
 
-        return $exercicio;
+        return ['img'=>true];
     }
 }
